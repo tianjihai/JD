f46572fe5fc9b6549549cc60ec67794b2c401f3a
tianjihai <jihai123@vip.qq.com>
2018-07-26 14:03:12 +0800

    小圆点修改2

--- js/main.js
+++ js/main.js
@@ -29,26 +29,26 @@ $(document).ready(function () {
         $(".goodsPic1").fadeIn(150);
         $(".goodsPic2").hide();
         $(".goods1").css('background-color', 'red').css('border','0px solid darkgray').css('box-shadow','0 0 5px red');
-        $(".goods2").css('background-color', 'white').css('border','1px solid darkgray').css('box-shadow','none');
+        $(".goods2").css('background-color', 'white').css('border','2px solid darkgray').css('box-shadow','none');
     });
     $(".goods2").mouseover(function () {
         $(".goodsPic2").fadeIn(150);
         $(".goodsPic1").hide();
         $(".goods2").css('background-color', 'red').css('border','0px solid darkgray').css('box-shadow','0 0 5px red');
-        $(".goods1").css('background-color', 'white').css('border','1px solid darkgray').css('box-shadow','none');
+        $(".goods1").css('background-color', 'white').css('border','2px solid darkgray').css('box-shadow','none');
     });
 
     $(".goodsListLeft").mouseover(function () {
         $(".goodsList1").fadeIn(200);
         $(".goodsList2").hide();
-        $(".goodsListLeft").css('color', 'red');
-        $(".goodsListRight").css('color', 'gray');
+        $(".goodsListLeft").css('background-color', 'red').css('border','0px solid darkgray').css('box-shadow','0 0 5px red');
+        $(".goodsListRight").css('background-color', 'white').css('border','2px solid darkgray').css('box-shadow','none');
     });
     $(".goodsListRight").mouseover(function () {
         $(".goodsList2").fadeIn(200);
         $(".goodsList1").hide();
-        $(".goodsListRight").css('color', 'red');
-        $(".goodsListLeft").css('color', 'gray');
+        $(".goodsListRight").css('background-color', 'red').css('border','0px solid darkgray').css('box-shadow','0 0 5px red');
+        $(".goodsListLeft").css('background-color', 'white').css('border','2px solid darkgray').css('box-shadow','none');
     });
 
 
@@ -93,24 +93,24 @@ $(document).ready(function () {
         $(".TeseUl2").hide();
         $(".TeseUl3").hide();
         $(".Tese11").css('background-color', 'red').css('border','0px solid darkgray').css('box-shadow','0 0 5px red');
-        $(".Tese22").css('background-color', 'white').css('border','1px solid darkgray').css('box-shadow','none');
-        $(".Tese33").css('background-color', 'white').css('border','1px solid darkgray').css('box-shadow','none');
+        $(".Tese22").css('background-color', 'white').css('border','2px solid darkgray').css('box-shadow','none');
+        $(".Tese33").css('background-color', 'white').css('border','2px solid darkgray').css('box-shadow','none');
     });
     $(".Tese22").mouseover(function () {
         $(".TeseUl2").fadeIn(150);
         $(".TeseUl1").hide();
         $(".TeseUl3").hide();
         $(".Tese22").css('background-color', 'red').css('border','0px solid darkgray').css('box-shadow','0 0 5px red');
-        $(".Tese11").css('background-color', 'white').css('border','1px solid darkgray').css('box-shadow','none');
-        $(".Tese33").css('background-color', 'white').css('border','1px solid darkgray').css('box-shadow','none');
+        $(".Tese11").css('background-color', 'white').css('border','2px solid darkgray').css('box-shadow','none');
+        $(".Tese33").css('background-color', 'white').css('border','2px solid darkgray').css('box-shadow','none');
     });
     $(".Tese33").mouseover(function () {
         $(".TeseUl3").fadeIn(150);
         $(".TeseUl1").hide();
         $(".TeseUl2").hide();
         $(".Tese33").css('background-color', 'red').css('border','0px solid darkgray').css('box-shadow','0 0 5px red');
-        $(".Tese11").css('background-color', 'white').css('border','1px solid darkgray').css('box-shadow','none');
-        $(".Tese22").css('background-color', 'white').css('border','1px solid darkgray').css('box-shadow','none');
+        $(".Tese11").css('background-color', 'white').css('border','2px solid darkgray').css('box-shadow','none');
+        $(".Tese22").css('background-color', 'white').css('border','2px solid darkgray').css('box-shadow','none');
     });
 
 
